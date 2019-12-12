561b3550fd196b98ef8ec8ed3cfef218a92e0e3e
jeff3071 <jeff563715@gmail.com>
2019-12-13 01:35:58 +0800

    增加移除表情符號

--- model/analysis.py
+++ model/analysis.py
@@ -1,8 +1,13 @@
 import jieba
 import numpy as np
+import flask
+from flask import render_template
+import re
 
 
 def main(article1, article2):
+    remove_emoji(article1)
+    remove_emoji(article2)
     seg_list1 = list(jieba.cut(article1,cut_all=False))
     seg_list2 = list(jieba.cut(article2, cut_all=False))
     
@@ -11,7 +16,7 @@ def main(article1, article2):
 
     seg_list1_wordvec = []
     seg_list2_wordvec = []
-    
+    dicts.remove('\r\n')
     for key in dicts:
         seg_list1_wordvec.append(seg_list1.count(key))
         seg_list2_wordvec.append(seg_list2.count(key))
@@ -26,5 +31,22 @@ def main(article1, article2):
 
     result = dot / (np1_array_sqrt * np2_array_sqrt)
 
-    return "Cos相似值: " + str(result)
+    return  render_template('ans.html', cosans = str(result), dict = str(dicts), article1 = article1, article2= article2)
 
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                         
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U00002702-\U000027B0"
+                u'\U00010000-\U0010ffff'
+                u"\u200d"
+                u"\u2640-\u2642"
+                u"\u2600-\u2B55"
+                u"\u23cf"
+                u"\u23e9"
+                u"\u231a"
+                u"\u3030"
+                u"\ufe0f"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
